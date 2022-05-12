b9d5c236308780e88f0c6ab16c34357deb6e3b57
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-05-12 07:22:49 +0000

    commit

--- EDA.py
+++ EDA.py
@@ -1,6 +1,15 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-try:
-    f = open('conversiones(4).csv')
-  
+conversion = pd.read_csv("conversion(4).csv")
+navegacion = pd.read_csv("navegacion(4).csv")
+
+navegacion = navegacion.dropna(subset = ["url_landing"])
+
+print("Este es el nuevo dataset: ")
+print(navegacion)
+
+csv_fusionado = pd.concat(map(pd.read_csv, [conversion, navegacion]), ignore_index=True)
+print(csv_fusionado)
+
+
