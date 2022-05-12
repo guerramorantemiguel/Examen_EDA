6f1ef6b49e19ef2ab09d17f8419f898d9d1d226f
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-05-12 07:33:41 +0000

    commit

--- EDA.py
+++ EDA.py
@@ -4,18 +4,20 @@ import matplotlib.pyplot as plt
 conversion = pd.read_csv("conversion(4).csv")
 navegacion = pd.read_csv("navegacion(4).csv")
 
-navegacion = navegacion.dropna(subset = ["url_landing"])
-conversion = conversion.dropna(subset = [""])
+navegacion = navegacion.dropna(subset = ["gclid"])
+conversion = conversion.dropna(subset = ["result"])
 
-print("Este es el nuevo dataset: ")
+print("Estos son los nuevos datasets: ")
 print(navegacion)
+print(conversion)
 
 df = pd.read_csv("navegacion(4).csv",  header=0,  sep = ",")
-for group in df.groupby(df["url_landing"]):
+for group in df.groupby(df["gclid"]):
     group[1].to_csv("{}.csv".format(group[0]), sep=',', index=False)
 
 
 csv_fusionado = pd.concat(map(pd.read_csv, [conversion, navegacion]), ignore_index=True)
 print(csv_fusionado)
+
 
 
