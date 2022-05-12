f04dbccd35bab4f10a703fb914f1b0ca572f89e0
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-05-12 07:28:17 +0000

    commit

--- EDA.py
+++ EDA.py
@@ -10,9 +10,12 @@ conversion = conversion.dropna(subset = [""])
 print("Este es el nuevo dataset: ")
 print(navegacion)
 
-csv_fusionado = pd.concat(map(pd.read_csv, [conversion, navegacion]), ignore_index=True)
-print(csv_fusionado)
-
 df = pd.read_csv("navegacion(4).csv",  header=0,  sep = ",")
 for group in df.groupby(df["url_landing"]):
     group[1].to_csv("{}.csv".format(group[0]), sep=',', index=False)
+
+
+csv_fusionado = pd.concat(map(pd.read_csv, [conversion, navegacion]), ignore_index=True)
+print(csv_fusionado)
+
+
