f41e0d1bb2d926d5be508fb854a998d2135a4b47
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-05-12 07:24:20 +0000

    commit

--- EDA.py
+++ EDA.py
@@ -12,4 +12,6 @@ print(navegacion)
 csv_fusionado = pd.concat(map(pd.read_csv, [conversion, navegacion]), ignore_index=True)
 print(csv_fusionado)
 
-
+df = pd.read_csv("navegacion(4).csv",  header=0,  sep = ",")
+for group in df.groupby(df["url_landing"]):
+    group[1].to_csv("{}.csv".format(group[0]), sep=',', index=False)
